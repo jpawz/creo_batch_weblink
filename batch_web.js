8f3d7ec28514f1caeca392b7f11b1b40e32728a9
jpawz <jpawz@outlook.com>
2020-09-17 21:27:54 +0200

    export to pdf implemented, bugs fixed

--- batch_web.js
+++ batch_web.js
@@ -28,10 +28,6 @@ function getAllDrws() {
       );
     drwsList = drwsList + "\n";
   }
-
-  document.getElementById("drawings").value = drwsList;
-  document.getElementById("filescount").innerHTML = "" + (index + 1) + " rysunków";
-  document.getElementById("workingdir").innerHTML = session.GetCurrentDirectory();
 }
 
 function convertToDxf() {
@@ -40,9 +36,9 @@ function convertToDxf() {
   var wpwl = pfcGetScript();
   document.pwl = wpwl;
   
-  document.getElementById("workingdir").innerHTML = session.GetCurrentDirectory();
-  
   var drawings = document.getElementById("drawings").value.split(/\b\s+/);
+  var expInstructions = pfcCreate("pfcDXFExportInstructions").Create();
+
   var index;
   for (index = 0; index < drawings.length; ++index) {
     model = document.pwl.pwlMdlOpen(drawings[index], "", true);
@@ -51,9 +47,44 @@ function convertToDxf() {
       drawings[index],
       pfcCreate("pfcModelType").MDL_DRAWING
     );
-    var expInstructions = pfcCreate("pfcDXFExportInstructions").Create();
     var outputName = drawings[index].substring(0, drawings[index].length - 3);
     drawing.Export(outputName + "dxf", expInstructions);
     document.pwl.pwlMdlErase(drawings[index]);
   }
+
+  alert("koniec");
+}
+
+
+function convertToPdf() {
+  if (!pfcIsWindows())
+    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+  var wpwl = pfcGetScript();
+  document.pwl = wpwl;
+   
+  var drawings = document.getElementById("drawings").value.split(/\b\s+/);
+  var expInstructions = pfcCreate("pfcPDFExportInstructions").Create();
+  var pdfOptions = pfcCreate("pfcPDFOptions");
+  var pdfLauncher = pfcCreate("pfcPDFOption").Create();
+  pdfLauncher.OptionType = (pfcCreate("pfcPDFOptionType").PDFOPT_LAUNCH_VIEWER);
+  var launcherSet = false;
+  var pdfArg = pfcCreate("MpfcArgument").CreateBoolArgValue(launcherSet);
+  pdfLauncher.OptionValue = pdfArg;
+  pdfOptions.Append(pdfLauncher);
+  expInstructions.Options = pdfOptions;
+  
+  var index;
+  for (index = 0; index < drawings.length; ++index) {
+    model = document.pwl.pwlMdlOpen(drawings[index], "", true);
+    var session = pfcGetProESession();
+    var drawing = session.GetModel(
+      drawings[index],
+      pfcCreate("pfcModelType").MDL_DRAWING
+    );
+    var outputName = drawings[index].substring(0, drawings[index].length - 3);
+    drawing.Export(outputName + "pdf", expInstructions);
+    document.pwl.pwlMdlErase(drawings[index]);
+  }
+
+  alert("koniec");
 }
